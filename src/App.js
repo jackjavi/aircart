07d22ce79d02d774faf75bc4f016a69393caf919
jackjavi <jackmtembete@gmail.com>
2022-06-11 20:08:11 +0300

    favicon

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Homepage from "./pages/Homepage";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="app">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Aircart</title>
+        <link rel="canonical" href="aircart-test.netlify.app" />
+        <meta
+          name="description"
+          content="The fastest way to sell digital products online"
+        />
+      </Helmet>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
