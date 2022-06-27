120e74c28124d1592c363895f5a017efd880c5aa
jackjavi <jackmtembete@gmail.com>
2022-06-27 11:21:17 +0300

    Refactored code

--- src/components/signUpComponents/SignUpContent.js
+++ src/components/signUpComponents/SignUpContent.js
@@ -20,14 +20,13 @@ const SignUpRight = () => {
   React.useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
-      client_id:
-        "979163570670-dsduftnkmikruf0vjqqn7bt067c8kpaf.apps.googleusercontent.com",
+      client_id: `process.env.GOOGLE_AUTH`,
       callback: handleCallbackResponse,
     });
 
     google.accounts.id.renderButton(document.getElementById("signin-div"), {
       theme: "outline",
-      size: "large",
+      size: "medium",
     });
   }, []);
 
@@ -43,17 +42,20 @@ const SignUpRight = () => {
         </div>
 
         <div className="sign-up-left-main-left">
-          <h3>Sell your Music in seconds</h3>
-          <p className="sign-up-main-left-p-1">
-            Create your product in the simple steps, and when you are ready
-            publish, start selling and earning.
-          </p>
-          <p className="sign-up-main-left-p-2">
-            <span className="sign-up-main-left-span-1">
-              Broken Pieces by Lila Fakau
-            </span>
-            <span className="sign-up-main-left-span-2">Get music</span>
-          </p>
+          <div className="sign-up-top"></div>
+          <div className="sign-up-bottom">
+            <h3>Sell your Music in seconds</h3>
+            <p className="sign-up-main-left-p-1">
+              Create your product in the simple steps, and when you are ready
+              publish, start selling and earning.
+            </p>
+            <p className="sign-up-main-left-p-2">
+              <span className="sign-up-main-left-span-1">
+                Broken Pieces by Lila Fakau
+              </span>
+              <span className="sign-up-main-left-span-2">Get music</span>
+            </p>
+          </div>
         </div>
       </div>
       <div className="sign-up-right">
@@ -62,47 +64,61 @@ const SignUpRight = () => {
         </div>
 
         <div className="sign-up-right-main">
-          <h3 className="sign-up-right-main-title">Get started with aircart</h3>
-          <p className="sign-up-right-main-desc">
-            Join more than 100,000 creators selling and earning with their
-            digital products.
-          </p>
-          <div className="sign-up-right-buttons-div">
-            {/* <GoogleLogin />
+          <div className="sign-up-right-content">
+            <div className="content-top">
+              <div>
+                <h3 className="sign-up-right-main-title">
+                  Get started with aircart
+                </h3>
+              </div>
+              <div>
+                {" "}
+                <p className="sign-up-right-main-desc">
+                  Join more than 100,000 creators selling and earning with their
+                  digital products.
+                </p>
+              </div>
+
+              <div className="sign-up-right-buttons-div">
+                {/* <GoogleLogin />
             <button className="sign-up-right-button">
               <span className="google-icon-text">
                 <img height="35%" src={GoogleIcon} alt="google-icon" />
                 Continue with Google
               </span>
             </button>*/}
-            <div id="signin-div"></div>
-            <FacebookLogin
-              appId={process.env.FACEBOOK_AUTH}
-              autoLoad={true}
-              fields="name,email,picture"
-              callback={responseFacebook}
-              cssClass="my-facebook-button-class"
-              icon="fa-facebook"
-            />
+                <div id="signin-div"></div>
+                <FacebookLogin
+                  appId="979163570670-ra7c4tdl0gd6ho7bjeadn53i9pakc1r7.apps.googleusercontent.com"
+                  autoLoad={true}
+                  fields="name,email,picture"
+                  callback={responseFacebook}
+                  cssClass="my-facebook-button-class"
+                  icon="fa-facebook"
+                />
 
-            {/*<FacebookLoginComponent />
+                {/*<FacebookLoginComponent />
             <button className="sign-up-right-button">
               <span className="facebook-icon-text">
                 <img height="35%" src={FacebookIcon} alt="google-icon" />
                 Continue with Facebook
               </span>
             </button>*/}
-          </div>
-          <div className="sign-up-right-hr-div">
-            <div>
-              <hr />
+              </div>
+              <div className="sign-up-right-hr-div">
+                <div>
+                  <hr />
+                </div>
+                <div className="or-div">or</div>
+                <div>
+                  <hr />
+                </div>
+              </div>
             </div>
-            <div className="or-div">or</div>
-            <div>
-              <hr />
+            <div className="content-bottom">
+              <SignUpRightForm />
             </div>
           </div>
-          <SignUpRightForm />
         </div>
       </div>
     </div>
--- src/components/signUpComponents/SignUpRightForm.js
+++ src/components/signUpComponents/SignUpRightForm.js
@@ -22,37 +22,40 @@ const SignUpRightForm = () => {
   };
   return (
     <form className="sign-up-form" onSubmit={handleSubmit}>
-      <div className="sign-up-email-div">
-        <label htmlFor="email">Email</label>
-        <input onChange={handleChange} type="email" name="email" id="email" />
-      </div>
+      <div className="form-top">
+        <div className="sign-up-email-div">
+          <label htmlFor="email">Email</label>
+          <input onChange={handleChange} type="email" name="email" id="email" />
+        </div>
 
-      <div className="sign-up-email-div">
-        <label htmlFor="full-name">Full Name</label>
-        <input type="text" name="fullName" id="full-name" />
-      </div>
+        <div className="sign-up-email-div">
+          <label htmlFor="full-name">Full Name</label>
+          <input type="text" name="fullName" id="full-name" />
+        </div>
 
-      <div className="sign-up-email-div">
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <div className="sign-up-email-div">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
       </div>
+      <div className="form-bottom">
+        <div className="sign-up-agreement-div">
+          <p>
+            By creating an account, you agree to our
+            <span>Terms of Service </span>and
+            <span>Privacy & cookie statement.</span>
+          </p>
+        </div>
 
-      <div className="sign-up-agreement-div">
-        <p>
-          By creating an account, you agree to our
-          <span>Terms of Service </span>and
-          <span>Privacy & cookie statement.</span>
-        </p>
-      </div>
+        <div className="sign-up-btn">
+          <button type="submit">Create aircart account</button>
+        </div>
 
-      <div className="sign-up-btn">
-        <button type="submit">Create aircart account</button>
-      </div>
-
-      <div className="sign-up-alternate-text">
-        <p>
-          Already have an account?<span>Log In</span>
-        </p>
+        <div className="sign-up-alternate-text">
+          <p>
+            Already have an account?<span>Log In</span>
+          </p>
+        </div>
       </div>
     </form>
   );
